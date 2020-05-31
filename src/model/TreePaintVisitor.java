35c6a1e4efc5cc9e9d85106cf758b9b151a9336a
Kennethmokhethi <kennethmahlatsemokhethi@gmail.com>
2020-05-31 12:10:41 +0200

    Implemented the IPiantVisitor

---
+++ src/model/TreePaintVisitor.java
@@ -0,0 +1,30 @@
+package model;
+
+import csc2a.model.Rectangle;
+import csc2a.model.Triangle;
+import javafx.scene.canvas.GraphicsContext;
+
+public class TreePaintVisitor implements IPaintVisitor{
+
+	
+	private GraphicsContext graphicsContext = null;
+	
+	public void setGraphicsContext(GraphicsContext graphicsContext)
+	{
+		this.graphicsContext = graphicsContext;
+	}
+	
+	public void paint(Rectangle rectangle) {
+		graphicsContext.setFill(rectangle.getColor());
+		graphicsContext.fillRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
+		
+	}
+
+	
+	public void paint(Triangle triangle) {
+		
+		graphicsContext.setFill(triangle.getColor());
+		graphicsContext.fillPolygon(triangle.getXCoords(),triangle.getYCoords(),3);
+	}
+
+}
